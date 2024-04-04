748d551029be07c7ae38c868597d66fcd7deb74a
Adarsh Agnihotri <114830392+Adarshagnihotri0@users.noreply.github.com>
2024-04-04 14:03:51 +0000

    modified configuration and revised variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 14456
+let accountEmail = "adarsh@gmail.com"
+var accountPassword = "122345t6fg"
+accountCity = "Kanpur"
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
